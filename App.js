e2de07ba756c9036045fa5b3bb9e4f49be5690fe
vanirahul <122552826+vanirahul@users.noreply.github.com>
2024-01-19 18:15:20 +0530

    Context API

--- App.js
+++ App.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth.context';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
+  useEffect(()=> {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  if(storedUserLoggedInInformation === '1'){
+    setIsLoggedIn(true);
+  }
+  },[]);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
   };
 
   return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
+     
+      <AuthContext.Provider
+      value={{
+        isLoggedIn:isLoggedIn,
+        onLogout:logoutHandler
+      }}
+      >
+      <MainHeader  onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+      </AuthContext.Provider>
+     
   );
 }
 
